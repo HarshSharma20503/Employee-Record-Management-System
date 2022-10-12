5a589830f399fff93950f3dd55cda3e9532fe1c0
Harsh Sharma <99866531+HarshSharma20503@users.noreply.github.com>
2022-10-12 12:51:47 +0530

    Update source.cpp
    Added menu and welcome functions

--- source.cpp
+++ source.cpp
@@ -1,4 +1,5 @@
-#include <bits/stdc++.h>
+#include <iostream>
+#include <stdlib.h> //to use system("CLS")
 using namespace std;
 
 class node
@@ -141,16 +142,121 @@ class Employee_record
         root=prl;
         return prl;
     }
+    
+    node* rinsert(node* p,node *q)
+    {
+           
+    }
+    node* deletion(node* p,int key)
+    {
+        
+    }
+    node* search(node* p)
+    {
+        
+    }
+    
+    void insertEmployee()
+    {
+        
+    }
+    
+    void deleteEmployee()
+    {
+        
+    }
+    void searchEmployee()
+    {
+        
+    }
+    void modifyEmployee()
+    {
+        
+    }
+    void display()
+    {
+        
+    }
+    void displayRecord(node* p)
+    {
+        
+    }
 };
 
+void showMenu()
+{
+    system ("cls");
+    cout<<"+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++"<<endl;
+    cout<<"+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++"<<endl;
+    cout<<"\n********Enter your choice for what you want to do :********\n"<<endl;
+    cout<<"**<1> for inserting a new employee record ***\n";
+    cout<<"**<2> for deleting an employee record     ***\n";
+    cout<<"**<3> for searching an employee record    ***\n";
+    cout<<"**<4> for modifying an employee record    ***\n";
+    cout<<"**<5> for viewing all employees record    ***\n";
+    cout<<"**<0> for exiting the program             ***\n";
+    cout<<"\n\n+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++"<<endl;
+    cout<<"+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++"<<endl;
+    cout<<"\nChoice: ";
+    int choice;
+    choi:
+    cin>>choice;
+    Employee_record obj;
+    switch(choice)
+    {
+        case 1:
+        obj.insertEmployee();
+        break;
+        case 2:
+        obj.deleteEmployee();
+        break;
+        case 3:
+        obj.searchEmployee();
+        break;
+        case 4:
+        obj.modifyEmployee();
+        break;
+        case 5:
+        obj.display();
+        break;
+        case 0:
+        exit(0);
+        break;
+        default :
+        cout<<"Enter correct choice: ";
+        goto choi;
+    }
+}
 
+void Welcome()
+{
+    cout<<"+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++"<<endl;
+    cout<<"+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++"<<endl;
+    cout<<"**** Welcome to Employement Record Management System ****\n\n";
+    cout<<"*** How this system works ***"<<endl;
+    cout<<"*You will be given number for a particular task. which ever task you want to perform you can "<<endl;
+    cout<<" enter that number and system which guide you to perform that task."<<endl;
+    cout<<"*** Lets begin ***\n";
+    cout<<"**<1> for Entering the System             ***\n";
+    cout<<"**<0> for exiting the program             ***\n";
+    cout<<"\nChoice: ";
+    int choice;
+    choi:
+    cin>>choice;
+    if(choice==1)
+    showMenu();
+    else if(choice==0)
+    return ;
+    else
+    {
+        cout<<"Enter correct choice: ";
+        goto choi;
+    }
+}
 
-
-
-
+//program starts from here (MAIN FUNCTION)
 int main()
 {
-    cout<<"Hello World";
-
+    Welcome();
     return 0;
 }
